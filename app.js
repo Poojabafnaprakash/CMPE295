681dcccab530ddc7e6516245232b6078c1a0f67c
Poojabafnaprakash <poojabafnaprakash@gmail.com>
2017-09-01 12:49:29 -0700

    added app.json

--- app.js
+++ app.js
@@ -36,9 +36,7 @@ app.get('/', index.index);
 app.get('/users', user.list);
 app.get('/directions', directionsAPI.directions);
 
-//http.createServer(app).listen(app.get('port'), function(){
-//  console.log('Express server listening on port ' + app.get('port'));
-//});
+http.createServer(app).listen(app.get('port'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
+});
 
-http.createServer(app).listen(process.env.PORT || 443);
-//app.listen(process.env.PORT || 3000);
