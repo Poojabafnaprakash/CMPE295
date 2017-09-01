c967b1c4eb3f413e39383bc22a96e33792f27665
Poojabafnaprakash <poojabafnaprakash@gmail.com>
2017-09-01 13:55:53 -0700

    changed package,json for google maps dependancy

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ var express = require('express')
 var app = express();
 
 // all environments
-app.set('port', process.env.PORT || 443);
+app.set('port', process.env.PORT || 3000);
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 app.use(express.favicon());
@@ -31,7 +31,6 @@ if ('development' == app.get('env')) {
   app.use(express.errorHandler());
 }
 
-//app.get('/', routes.index);
 app.get('/', index.index);
 app.get('/users', user.list);
 app.get('/directions', directionsAPI.directions);
