badcc02473b862f63022aabc9a0e1810ceb32be8
Poojabafnaprakash <poojabafnaprakash@gmail.com>
2017-10-02 23:12:16 -0700

    code to send Anusha for steps between source and dest

--- app.js
+++ app.js
@@ -6,14 +6,21 @@
 var express = require('express')
   , http = require('http')
   , path = require('path')
+  , session = require('client-sessions')
   , routes = require('./routes')
   , directionsAPI = require('./routes/directionsAPI')
   , index = require('./routes/index')
+  , login = require("./routes/login")
   , user = require('./routes/user');
 
-
-
 var app = express();
+
+app.use(session({   
+	cookieName: 'session',    
+	secret: 'cmpe295',    
+	duration: 30 * 60 * 1000,    
+	activeDuration: 5 * 60 * 1000
+}));
 
 // all environments
 app.set('port', process.env.PORT || 3000);
@@ -37,6 +44,12 @@ app.get('/distance', directionsAPI.distance);
 app.get('/time', directionsAPI.totalTime);
 app.get('/latlng', directionsAPI.latLng);
 app.use('/cronJob', directionsAPI.cronJob);
+
+app.post('/api/userInput', directionsAPI.userInput);
+app.post('/api/checklogin', login.checkLogin);
+app.post('/api/register', login.register);
+app.get('/api/homepage',login.redirectToHomepage);
+app.post('/api/logout',login.logout);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
--- routes/directionsAPI.js
+++ routes/directionsAPI.js
@@ -5,6 +5,9 @@ var googleMapsClient = require('@google/maps').createClient({
 
 var fs = require('fs');
 var Combinatorics = require('js-combinatorics');
+var Client = require('node-rest-client').Client;
+var client = new Client();
+
 
 exports.distance = function(req, res) {
 	googleMapsClient.directions({
@@ -13,7 +16,7 @@ exports.distance = function(req, res) {
 		mode : req.param("mode")
 	}, function(err, response) {
 		if (!err) {
-			res.send(response.json.routes[0].legs[0].distance);
+			res.send(response.json.routes[0].legs[0].steps);
 		}
 	});
 };
@@ -31,23 +34,162 @@ exports.totalTime = function(req, res) {
 };
 
 exports.latLng = function(req, res) {
-	googleMapsClient
-			.directions(
-					{
-						origin : req.param("source"),
-						destination : req.param("destination"),
-						mode : req.param("mode")
-					},
-					function(err, response) {
-						if (!err) {
-							var latlng = {
-								"sourceLatLng" : response.json.routes[0].legs[0].start_location,
-								"destinationLatlng" : response.json.routes[0].legs[0].end_location
-							}
-							res.send(latlng);
-						}
-					});
+	console.log(req.body.source);
+	res.send("{success: 200}");
 };
+
+let getSteps = function(req) {
+	  return new Promise(function(resolve, reject) {
+		  googleMapsClient.directions({
+				origin : req.param("source"),
+				destination : req.param("destination"),
+				mode : req.param("mode")
+			}, function(err, response) {
+				if (!err) {
+					var finalSteps = []; var finalStepsObj = {}; var firstdirection = "";
+					for (var key in response.json.routes[0].legs[0].steps) {
+					    var htmlInstruction = response.json.routes[0].legs[0].steps[key].html_instructions;
+					    var regex = /<b>\s*(.*?)\s*<\/b>/g;
+					    var matches = [];
+					    while (m = regex.exec(htmlInstruction)) {
+					    	matches.push(m[1]);
+					    }
+					    if(firstdirection === "") {
+					    	firstdirection = mapDirections(matches[0], "");
+					    }
+					    
+					    if(matches.length > 1 && !("maneuver" in response.json.routes[0].legs[0].steps[key]) ) {
+					    	finalStepsObj = {
+					    			"streetName" : matches[1],
+					    			"direction" : firstdirection,
+					    			"distanceCovered" : response.json.routes[0].legs[0].steps[key].distance.text,
+					    			"timeTaken" : response.json.routes[0].legs[0].steps[key].duration.text,
+					    			"startTime" : response.json.routes[0].legs[0].steps[key].duration.text
+					    	};
+					    } else if(matches.length > 1) {
+					    	firstdirection = mapDirections(firstdirection, response.json.routes[0].legs[0].steps[key].maneuver);
+					    	finalStepsObj = {
+					    			"streetName" : matches[1],
+					    			"direction" : firstdirection,
+					    			"distanceCovered" : response.json.routes[0].legs[0].steps[key].distance.text,
+					    			"timeTaken" : response.json.routes[0].legs[0].steps[key].duration.text,
+					    			"startTime" : response.json.routes[0].legs[0].steps[key].duration.text
+					    	};
+					    } else {
+					    	finalStepsObj = {
+					    			"streetName" : matches[0],
+					    			"direction" : firstdirection,
+					    			"distanceCovered" : response.json.routes[0].legs[0].steps[key].distance.text,
+					    			"timeTaken" : response.json.routes[0].legs[0].steps[key].duration.text,
+					    			"startTime" : response.json.routes[0].legs[0].steps[key].duration.text
+					    	};
+					    }
+					    finalSteps.push(finalStepsObj);
+					}
+					var steps = {
+							"Summary":[
+								{ "Start": response.json.routes[0].legs[0].start_address },
+								{ "End": response.json.routes[0].legs[0].end_address },
+								{ "Time": response.json.routes[0].legs[0].duration.text },
+								{ "StartTime": req.param("time") },
+								{ "Day": req.param("day") }
+							],
+							"steps": finalSteps
+					}
+					console.log(steps);
+					resolve(steps);
+				} else {
+					reject('Error getting response from Google');
+				}
+			});
+	});
+};
+
+
+
+let getCongestion = function(getStepsResult) {
+	return new Promise(function(resolve, reject) {
+		var args = {
+			    data: getStepsResult ,
+			    headers: { "Content-Type": "application/json" }
+		};
+		client.post("http://remote.site/rest/xml/method", args, function (data, response) {
+		    //console.log(data);
+		    resolve(data);
+		}); 
+	});
+};
+
+	let winIcecream = function(message) {
+	  return new Promise(function(resolve, reject) {
+	    resolve( message + ' won Icecream');
+	  });
+	};
+
+//	cleanRoom().then(function(result){
+//		return removeGarbage(result);
+//	}).then(function(result){
+//		return winIcecream(result);
+//	}).then(function(result){
+//		console.log('finished ' + result);
+//	})
+
+let mapDirections = function(direction, menuver) {
+	var newDirection;	
+	if(menuver == "turn-left") {
+		if(direction == "southeast") {
+			newDirection = "east";
+		} else if(direction == "southwest") {
+			newDirection = "south";
+		} else if(direction == "northwest") {
+			newDirection = "west";
+		} else if(direction == "northeast") {
+			newDirection = "north";
+		} else if(direction == "north") {
+			newDirection = "west";
+		} else if(direction == "south") {
+			newDirection = "east";
+		} else if(direction == "east") {
+			newDirection = "north";
+		} else if(direction == "west") {
+			newDirection = "south";
+		} else {
+			newDirection = direction;
+		}
+	} else if(menuver == "turn-right") {
+		if(direction == "southeast") {
+			newDirection = "south";
+		} else if(direction == "southwest") {
+			newDirection = "west";
+		} else if(direction == "northwest") {
+			newDirection = "north";
+		} else if(direction == "northeast") {
+			newDirection = "east";
+		} else if(direction == "north") {
+			newDirection = "east";
+		} else if(direction == "south") {
+			newDirection = "west";
+		} else if(direction == "east") {
+			newDirection = "south";
+		} else if(direction == "west") {
+			newDirection = "north";
+		} else {
+			newDirection = direction;
+		}
+	} else {
+		newDirection = direction;
+	}
+	return newDirection;
+}
+	
+exports.userInput = function(req, res) {
+	getSteps(req).then(function(result){
+		return getCongestion(result)	
+	}).then(function(congestionResult){
+		res.send(congestionResult);
+	})	
+};
+
 
 exports.directions = function(req, res) {
 	googleMapsClient.directions({
@@ -69,19 +211,20 @@ exports.cronJob = function(req, res) {
 			"\n");
 	var listOfObjects = [];
 	var responseObj = [];
-	for(var i = 0; i < 3; i++) {
-		for(var j = 0; j < 3; j++) {
+	for(var i = 0; i < 1; i++) {
+		for(var j = 0; j < arr.length - 350; j++) {
 			if(arr[i] === arr[j]) continue;
 			var obj = {};
 	    	obj['src'] = arr[i];
 			obj['dst'] = arr[j];
 			listOfObjects.push(obj);
-		}		
+		}	
+		console.log(Object.keys(listOfObjects).length);
 	}
 	var promises = [];
 	listOfObjects.forEach(function (arrayItem) {	   
 		var cronJobObj = {};
-		console.log(arrayItem);
+		//console.log(arrayItem);
 		promises.push(
 		googleMapsClient.directions({
 			origin : arrayItem.src + ', San Jose, CA',
@@ -103,7 +246,7 @@ exports.cronJob = function(req, res) {
 			responseObj.push(cronJobObj);
 		  })
 		  .catch((err) => {
-		    console.log(err);
+		    console.log(arrayItem.src + " " + arrayItem.dst+ " " +err);
 		  }));		
 	});
 	
