9806207c364281b40fd761d76954ecec09bdb7cc
Poojabafnaprakash <poojabafnaprakash@gmail.com>
2017-10-16 14:36:35 -0700

    minute

--- routes/cronJob.js
+++ routes/cronJob.js
@@ -9,8 +9,8 @@ var client = new Client();
 var cron = require('node-cron');
 
 
-var task = cron.schedule('*/30 * * * *', function() {
-//var task = cron.schedule('*/2 * * * *', function() {
+//var task = cron.schedule('*/30 * * * *', function() {
+var task = cron.schedule('*/1 * * * *', function() {
 	var arr = fs.readFileSync(
 	'../cronjobstreets.json');
 var responseObj = [];
