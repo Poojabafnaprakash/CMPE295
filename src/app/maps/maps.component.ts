37ec47db5dd1a16a9d0788aafc9e520cbae26981
Poojabafnaprakash <poojabafnaprakash@gmail.com>
2017-11-29 22:08:32 -0800

    added select option filter

--- src/app/maps/maps.component.ts
+++ src/app/maps/maps.component.ts
@@ -27,6 +27,9 @@ export class MapsComponent implements OnInit {
   predictionResult: PredictedResults;
   latLngResponse: LatLng;
   showCongestionRateTable: boolean = false;
+  testFilter:any;
+  sources:any;
+  destinations:any;
 
   constructor(private userInputService: UserInputService) { }
 
@@ -41,6 +44,44 @@ export class MapsComponent implements OnInit {
     var map = new google.maps.Map(document.getElementById('map'), mapOptions);
 
     this.userInput = new UserInput(1, "", "", "", "");
+
+    this.testFilter = "hi";
+    this.sources = ["Casa Verde Street ", "Alum Rock Park", "San Jose State University", "Avaya Stadium", "DMV San Jose", "MapR Technologies, 350 Holger Way", "Winchester Mystery House", "N 1st Street ", "Santana Row", "Costco Wholesale, 1709 Automation Pkwy, San Jose, CA 95131", "Downtown San Jose", "1302,The Alameda", "Mineta San Jose International Airport"];
+    this.destinations = [""];
+  }
+
+  modelChanged(newObj: any) {
+    if(newObj == "Casa Verde Street") {
+      this.destinations = ["Mineta San Jose International Airport", "San Jose State University", "Costco Wholesale, 1709 Automation Pkwy, San Jose, CA 95131"];
+    } else if(newObj == "Alum Rock Park") {
+      this.destinations = ["San Jose State University"];
+    } else if(newObj == "San Jose State University") {
+      this.destinations = ["1302,The Alameda", "Alum Rock Park", "Casa Verde Street ", "Mineta San Jose International Airport", "Costco Wholesale, 1709 Automation Pkwy, San Jose, CA 95131", "Avaya Stadium", "DMV San Jose", "MapR Technologies, 350 Holger Way", "Winchester Mystery House", "Santana Row"];
+    } else if(newObj == "Mineta San Jose International Airport") {
+      this.destinations = ["1302,The Alameda", "Casa Verde Street ", "San Jose State University"];
+    } else if(newObj == "Costco Wholesale, 1709 Automation Pkwy, San Jose, CA 95131") {
+      this.destinations = ["San Jose State University"];
+    } else if(newObj == "Avaya Stadium") {
+      this.destinations = ["San Jose State University"];
+    } else if(newObj == "DMV San Jose") {
+      this.destinations = ["San Jose State University"];
+    } else if(newObj == "MapR Technologies, 350 Holger Way") {
+      this.destinations = ["San Jose State University"];
+    } else if(newObj == "Winchester Mystery House") {
+      this.destinations = ["San Jose State University"];
+    } else if(newObj == "N 1st Street") {
+      this.destinations = ["Santana Row", "Downtown San Jose"];
+    } else if(newObj == "Santana Row") {
+      this.destinations = ["N 1st Street ", "San Jose State University"];
+    } else if(newObj == "Costco Wholesale, 1709 Automation Pkwy, San Jose, CA 95131") {
+      this.destinations = ["Casa Verde Street "];
+    } else if(newObj == "Downtown San Jose") {
+      this.destinations = ["N 1st Street "];
+    } else if(newObj == "1302,The Alameda") {
+      this.destinations = ["Mineta San Jose International Airport", "San Jose State University"];
+    } else {
+      this.destinations = ["Choose a proper source"];
+    }
   }
 
   calculateAndDisplayRoute(directionsService: any, directionsDisplay: any, pointA: any, pointB: any) {
@@ -60,6 +101,7 @@ export class MapsComponent implements OnInit {
   }
 
   setFav(source: string, destination: string, dayOfWeek: string, timeOfDay: string) {
+    console.log(this.userInput);
     this.userInputService.setFavorite(this.userInput)
       .subscribe(successCode => {
         window.alert('Added Favorite' + successCode);
